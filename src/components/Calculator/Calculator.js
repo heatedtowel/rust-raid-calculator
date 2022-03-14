d75f33c4f9d2465b28dc72707124a44c0576896e
Jesse Monks <monksjesse@gmail.com>
2022-03-13 21:32:11 -0400

    updated wood values

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,19 +3,14 @@ import RaidCosts from '../../data/masterRaidCosts.json'
 
 const Calculator = () => {
 
+  console.log(RaidCosts)
+
   const [buildingMaterial, setBuildingMaterial] = useState('Wood')
   const [buildingPiece, setBuildingPiece] = useState('Wooden Door')
 
   return (
     <div>
-      {
-        RaidCosts.deployables.map((types) => (
-          <>
-            <h1>{types.material}</h1>
-            <h1>{types.buildingPiece}</h1>
-          </>
-        ))
-      }
+
     </div>
   )
 }
